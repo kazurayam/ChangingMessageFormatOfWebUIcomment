5e3f74b41630179cc869dc2760b32bfde78cf22a
kazurayam <kazuaki.urayama@gmail.com>
2019-04-27 21:54:49 +0900

    enabled logging into FILE

--- Keywords/com/kazurayam/ksbackyard/CustomLoggerFactory.groovy
+++ Keywords/com/kazurayam/ksbackyard/CustomLoggerFactory.groovy
@@ -11,6 +11,7 @@ import ch.qos.logback.classic.Logger
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder
 import ch.qos.logback.core.ConsoleAppender
+import ch.qos.logback.core.FileAppender
 
 public class CustomLoggerFactory {
 
@@ -27,8 +28,20 @@ public class CustomLoggerFactory {
 	 *       <pattern>%msg%n</pattern>
 	 *     </encoder>
 	 *   </appender>
+	 *   <appender name="FILE" class="ch.qos.logback.core.FileAppender">
+	 *     <file>testFile.log</file>
+     *     <append>true</append>
+     *     <!-- set immediateFlush to false for much higher logging throughput -->
+     *     <immediateFlush>true</immediateFlush>
+     *     <!-- encoders are assigned the type
+     *          ch.qos.logback.classic.encoder.PatternLayoutEncoder by default -->
+     *     <encoder>
+     *       <pattern>%-4relative [%thread] %-5level %logger{35} - %msg%n</pattern>
+     *     </encoder>
+	 *   </appender>
 	 *   <logger name="com.kms.katalon.core.keyword.builtin.CommentKeyword" level="info" additivity="false">
 	 *     <appender-ref ref="STDOUT-msgOnly" />
+	 *     <appender-ref ref="FILE" />
 	 *   </logger>
 	 * </configuration>
 	 * </PRE>
@@ -42,16 +55,31 @@ public class CustomLoggerFactory {
 		msgOnlyEncoder.setPattern("%msg%n")
 		msgOnlyEncoder.start()
 		//
+		PatternLayoutEncoder fileEncoder = new PatternLayoutEncoder()
+		fileEncoder.setContext(logCtx)
+		fileEncoder.setPattern("%-4relative [%thread] %-5level %logger{35} - %msg%n")
+		fileEncoder.start()
+		//
 		ConsoleAppender msgOnlyAppender = new ConsoleAppender()
 		msgOnlyAppender.setContext(logCtx)
 		msgOnlyAppender.setName("STDOUT-msgOnly")
 		msgOnlyAppender.setEncoder(msgOnlyEncoder)
 		msgOnlyAppender.start()
 		//
+		FileAppender fileAppender = new FileAppender()
+		fileAppender.setContext(logCtx)
+		fileAppender.setName("FILE")
+		fileAppender.setFile("testFile.log")
+		fileAppender.setAppend(true)
+		fileAppender.setImmediateFlush(true)
+		fileAppender.setEncoder(fileEncoder)
+		fileAppender.start()
+		//
 		Logger logger = logCtx.getLogger(CLASSNAME_COMMENT_KEYWORD)
 		logger.additive = false
 		logger.level = Level.INFO
 		logger.addAppender(msgOnlyAppender)
+		logger.addAppender(fileAppender)
 		//
 		return logger
 	}
