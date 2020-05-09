7ee9f30c8e30b58c6f6fafb24f8bdf8e201828e7
kazurayam <kazuaki.urayama@gmail.com>
2020-05-09 09:01:02 +0900

    add a feature of logging into a file

---
+++ Scripts/Example_customizeCommentKeyword4levelAndMsg2File/Script1588980513449.groovy
@@ -0,0 +1,12 @@
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
+File commentLog = new File("./comment.log")
+
+// make a patch to LogBack LoggerFactory object on the fly.
+// write messages by WebUI.comment(String) into File in the "level message" format.
+CustomKeywords.'com.kazurayam.ksbackyard.CustomLoggerFactory.customizeCommentKeyword4levelAndMsg2File'(commentLog)
+
+def arg0 = 2
+def arg1 = 3
+def result = arg0 + arg1
+WebUI.comment("arg0 + arg1 = ${result}")
--- Scripts/Example_customizeCommentKeyword4msgOnly2Console/Script1588980533191.groovy
+++ Scripts/Example_customizeCommentKeyword4msgOnly2Console/Script1588980533191.groovy
@@ -1,7 +1,8 @@
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
 // make a patch to LogBack LoggerFactory object runtime
-CustomKeywords.'com.kazurayam.ksbackyard.CustomLoggerFactory.customizeLogger4CommentKeyword'()
+// make messages by WebUI.comment(String) less verbose - message only to Console
+CustomKeywords.'com.kazurayam.ksbackyard.CustomLoggerFactory.customizeCommentKeyword4MsgOnly2Console'()
 
 def arg0 = 2
 def arg1 = 3
